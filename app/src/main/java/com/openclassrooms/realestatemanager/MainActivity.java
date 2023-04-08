b0f4449262da2fddc9df7b306b09c450010fec5d
Popp <poolparty30gmail.com>
2023-04-08 13:38:20 +0200

    main Authentication activity_main.xml

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -1,34 +1,89 @@
 package com.openclassrooms.realestatemanager;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textViewMain;
-    private TextView textViewQuantity;
-
+    private static final int RC_SIGN_IN = 123;
+    private Button emailButton, googleButton;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
-        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
-
-        this.configureTextViewMain();
-        this.configureTextViewQuantity();
     }
 
-    private void configureTextViewMain(){
-        this.textViewMain.setTextSize(15);
-        this.textViewMain.setText(R.string.the_property_coast);
+    private void setUpView() {
+        googleButton = findViewById(R.id.login_button_google);
+        emailButton = findViewById(R.id.login_button_email);
+        progressBar = findViewById(R.id.progressBar);
     }
 
-    private void configureTextViewQuantity(){
-        int quantity = Utils.convertDollarToEuro(100);
-        this.textViewQuantity.setTextSize(20);
-        this.textViewQuantity.setText(String.valueOf(quantity));
+    private void setupListeners() {
+        progressBar.setVisibility(View.INVISIBLE);
+
+        emailButton.setOnClickListener(view ->
+                signInBuilder(Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build()))
+        );
+        googleButton.setOnClickListener(view ->
+                signInBuilder(Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build()))
+        );
     }
+
+    private void signInBuilder(List<AuthUI.IdpConfig> providers) {
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN );
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // Show Toast with a message
+    private void showToast(String message) {
+        Toast.makeText(this, message, LENGTH_SHORT).show();
+    }
+
+    // Method that handles response after SignIn Activity close
+  /*  private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                viewModel.createUser();
+                showToast(String.valueOf(getString(R.string.connected)));
+                startHomeActivity();
+                progressBar.setVisibility(View.INVISIBLE);
+            } else if (response == null) {
+                showToast(String.valueOf(getString(R.string.notConnected)));
+            }
+        }
+    }*/
+
+
 }
+
